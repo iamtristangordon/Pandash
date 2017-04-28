ff294902e990a86a6fb6c4eb16056580293a9c3a
Tristan Gordon <tristan@tdgordon.com>
2017-04-28 09:41:45 -0500

    Add auth service

---
+++ src/app/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { Http } from '@angular/http';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+  login(credentials) {
+
+  }
+
+  logout(credentials) {
+
+  }
+}
